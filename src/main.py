82b6a49217aabae2f441be9bb51303127a60e3a1
Simon Prickett <simon@redislabs.com>
2023-10-06 22:15:05 +0100

    Work in progress.

--- src/main.py
+++ src/main.py
@@ -5,10 +5,22 @@ import time
 import urequests
 
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2, PEN_RGB565
+from pimoroni import RGBLED
 
-print(gc.mem_free())
+def create_display():
+    return PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, pen_type=PEN_RGB565, rotate=0)
 
-display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, pen_type=PEN_RGB565, rotate=0)
+def get_song_data(radio_station):
+    now_playing = urequests.get(f"https://rms.api.bbc.co.uk/v2/services/{radio_station}/segments/latest?experience=domestic&offset=0&limit=1", headers= {"User-Agent": "PicoW"}).json()    
+    status = now_playing["data"][0]["offset"]["label"]
+    artist = now_playing["data"][0]["titles"]["primary"]
+    song = now_playing["data"][0]["titles"]["secondary"]
+        
+    # TODO sanitize special characters out of these values...
+    return status, artist, song
+
+led = RGBLED(6, 7, 8)
+display = create_display()
 
 BLACK_PEN = display.create_pen(0, 0, 0)
 ORANGE_PEN = display.create_pen(250, 100, 0)
@@ -31,17 +43,15 @@ show_artist = True
 display = None
 gc.collect()
 
-now_playing = urequests.get("https://rms.api.bbc.co.uk/v2/services/bbc_radio_two/segments/latest?experience=domestic&offset=0&limit=1", headers= {"User-Agent": "PicoW"}).json()
-status = now_playing["data"][0]["offset"]["label"]
-artist = now_playing["data"][0]["titles"]["primary"]
-song = now_playing["data"][0]["titles"]["secondary"]
-# TODO Sanitize to be A-Z 0-9 and replace anything else with a space.
-now_playing = None
-gc.collect()
-
-display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, pen_type=PEN_RGB565, rotate=0)
+last_updated = time.ticks_ms()
+led.set_rgb(61, 21, 15)  
+status, artist, song = get_song_data("bbc_radio_two")
+led.set_rgb(0, 32, 0) 
+display = create_display()
 
 while True:
+    gc.collect()
+    
     display.set_pen(ORANGE_PEN)
     display.circle(245, 85, 60)
     display.set_pen(WHITE_PEN)
@@ -63,7 +73,20 @@ while True:
     display.update()
     
     time.sleep(3)
-    
+
     display.set_pen(BLACK_PEN)
     display.rectangle(0, 160, 320, 240)
     display.update()
+
+    ticks_now = time.ticks_ms()
+    
+    if time.ticks_diff(ticks_now, last_updated) > 30000:
+        print("time to update from api")
+        led.set_rgb(61, 21, 15)   
+        display = None
+        gc.collect()
+        status, artist, song = get_song_data("bbc_radio_two")
+            
+        display = create_display()
+        last_updated = time.ticks_ms()
+        led.set_rgb(0, 32, 0)   
